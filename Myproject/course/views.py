60c931232868779c40b9302e96384cf15853de6d
Muktadir-SoftWorks <muktadirr.info@gmail.com>
2024-11-11 06:57:28 +0600

    Day 4

--- Myproject/course/views.py
+++ Myproject/course/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def learn_django(request):
     return HttpResponse('<h1>Hello Django</h1>')
+
+def learn_python(request):
+    return render(request, 'course_python.html')
+
+def calculate(request):
+    return HttpResponse('I am calculator')
--- Myproject/fees/views.py
+++ Myproject/fees/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def pay_fees(request):
+    return HttpResponse("Semester fee is 40k")
+
+def learn_python(request):
+    return HttpResponse('<h1>Hello From Index page</h1>')
